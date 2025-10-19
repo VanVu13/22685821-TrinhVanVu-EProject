3376f09fc96a5823194f13bfbf98357b578eb2a1
VanVu <trinhvanvu@gmail.com>
2025-10-19 23:50:14 +0700

    Cap nhat ci/cd new5

--- product/src/routes/productRoutes.js
+++ product/src/routes/productRoutes.js
@@ -9,5 +9,5 @@ router.post("/", isAuthenticated, productController.createProduct);
 router.post("/buy", isAuthenticated, productController.createOrder);
 router.get("/", isAuthenticated, productController.getProducts);
 router.get("/order-status/:orderId", isAuthenticated, productController.getOrderStatus);
-router.get("/:id", isAuthenticated, productController.getProductById);
+// router.get("/:id", isAuthenticated, productController.getProductById);
 module.exports = router;
